b4ffba329a8cc8b5c7ee8351cf890312a91978e4
RazerXT <101035245+RazerXT@users.noreply.github.com>
2024-04-22 17:13:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '20227614'))
 API_HASH = environ.get('API_HASH', 'b9268f7d01884a3d740ba17d0ad8754b')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7029666210:AAF2ZAyRgUfjiLBSY9BAvAoOe5JmDxYuAmg")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6981281170:AAHNgyB7OmBch4ocrHHKgJBSNoajx9DGlM0")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
